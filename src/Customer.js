7663f1aea1f3e69ec22a03dc37482ddd2b96c485
shubnammingwal <shubhammingwal1986@gmail.com>
2024-03-31 11:40:38 +0530

    add customer feedback

--- src/Customer.js
+++ src/Customer.js
@@ -5,31 +5,32 @@ import pokerIcon from './icons/poker-icon.png';
 import customerImg from './images/customer-img.png';
 import leftQuote from './icons/left-quote.png';
 import rightQuote from './icons/right-quote.png';
+import arrow from './images/arrow.png';
 const Customer = () => {
   return (
     <div className="customer" id="review">
       <div className="pokerIconHead">
-        <img className="pokerIcon" src="./public/icons/poker-icon.png" alt="poker icon" />
+        <img className="pokerIcon" src={pokerIcon} alt="poker icon" />
         <h1>What Is Says Our Customer</h1>
       </div>
 
       <div className="customerBox">
         <div className="arrowBox leftArrow">
-          <img src="./public/images/arrow.png" alt="left arrow" />
+          <img src={arrow} alt="left arrow" />
         </div>
 
         <div className="customerImg">
-          <img src="./public/images/customer-img.png" alt="customer image" />
+          <img src={customerImg} alt="customer image" />
         </div>
 
         <h1>Jone Mark</h1>
-        <p><img src="./public/icons/left-quote.png" alt="left quote"/> Lorem ipsum dolor sit amet, <img src="./public/icons/right-quote.png" alt="right quote"/></p>
+        <p><img src={leftQuote} alt="left quote"/> Lorem ipsum dolor sit amet, <img src={rightQuote} alt="right quote"/></p>
         <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim
           veniam, quis nostrud exercitation ullamco laboris ut aliquip ex ea commodo
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
         <div className="arrowBox rightArrow">
-          <img src="./public/images/arrow.png" alt="right arrow" />
+          <img src={arrow} alt="right arrow" />
         </div>
       </div>
     </div>
